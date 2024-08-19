57e8499f95493d3b0a07fdf7d37bddf0e6cd07ef
lovkumawat <kumawatlov6@gmail.com>
2024-08-19 19:33:45 +0530

    login commit

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -2,6 +2,10 @@ const User=require("../models/User");
 const OTP=require("../models/OTP");
 const otpGenerator=require("otp-generator");
 const bcrypt=require("bcrypt");
+const jwt=require("jsonwebtoken");
+const mailSender = require("../utils/mailSender")
+require("dotenv").config();
+
 
 
 // sendOTP
@@ -174,15 +178,148 @@ exports.signUp=async(req,res)=>{
 }
 
 
-
-
-
-
-
-
 //Login
+
+exports.login=async(req,res)=>{
+
+    try{
+        // get data from req body
+        const {email,password}=req.body;
+
+        // validation data
+        if(!email||!password){
+            return res.status(403).json({
+                success:false,
+                message:"All field are required,please try again",
+            });
+        }
+        //user check exits or not
+        const user=await User.findOne({email}).populate("additionalDetails");
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"User is not registered ,please signup first",
+            });
+        }
+        //generate JWT,after password matchinf
+      
+
+        if(await bcrypt.compare(password,user.password)){
+            // password match
+            const payload={
+                email:user.email,
+                id:user._id,
+                role:user.role
+            }
+
+            let token=jwt.sign(payload,
+                process.env.JWT_SECRET,
+                {
+                    expiresIn:"2h",
+                }
+              ); 
+                // token added
+                user=user.toObject();
+                user.token=token;
+                // remove password from user object
+                // not in database 
+                user.password=undefined;
+
+
+                // create cookie and send response 
+                
+              // creating option
+              const options={
+
+                // expire date: from till now to 3 date 
+                expire:new Date(Date.now()+3*24*60*60*1000),
+                // client side pe isee ham access nhi kr payenge 
+                httpOnly:true,
+              }
+
+              //  cookie:name,value,options 
+              res.cookie("token",token,options).status(200).json({
+                success:true,
+                token,
+                user,
+                message:'User is Logged in successfully ',
+              });
+
+                }else{
+                    // password do not match
+                    return res.status(401).json({
+                        success:false,
+                        message:"Password Incorrect",
+                    });
+    
+
+
+
+
+                }
+            }
+            catch(error){
+                console.log(error);
+                return res.status(500).json({
+                    success:false,
+                    message:'Login Failure'
+                });
+
+
+            }
+ }
 
 
 
 
 // changePassword
+// exports.changePassword=async(req,res)=>{
+
+//     try{
+//             // get data from req body
+//     const userDetails= await User.findById(req.user.id)
+//     // get olsPassword,newPassword,confirmPassword
+//     const {oldPassword,newPassword}=req.body
+
+//   // validate data
+//   if(!(await bcrypt.compare(oldPassword,userDetails.password))){
+//             // If old password does not match, return a 401 (Unauthorized) error
+//     return res
+//     .status(401)
+//     .json({ success: false, message: "The password is incorrect" })
+//   }
+
+//   // encrypt password
+//   const encryptedPassword=await bcrypt.hash(newPassword,10)
+//    // update pwd in DB
+//   const updatedPassword=await User.findByIdAndUpdate(req.user.id,{password:encryptedPassword},{new:true})
+//   // send mail--Password updated
+
+
+
+
+
+      
+  
+
+
+
+
+
+
+
+
+
+
+
+ 
+  
+//   //return response
+
+//     }catch(error){
+
+//     }
+
+// }
+
+
